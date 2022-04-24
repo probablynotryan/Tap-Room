05f7b48e943e811ebf776ab6815f1039389d3905
Ryan Bass <theryanbass@gmail.com>
2022-04-24 14:45:00 -0700

    add add soda form

--- src/components/AddSoda.js
+++ src/components/AddSoda.js
@@ -1,9 +1,32 @@
 import React from "react";
+import { v4 } from 'uuid';
+import SodaControl from './SodaControl';
 
 export default function AddSoda() {
+
+  function handleNewTicketFormSubmission(e) {
+    e.preventDefault();
+    console.log(e.target.name.value);
+    console.log(e.target.brand.value);
+  }
+
   return (
     <>
-      <p>test</p>
+      <form>
+        <input
+          type = "text"
+          name = "brand"
+          placeholder = "Brand" />
+        <input
+          type = "text"
+          name = "name"
+          placeholder = "Name of Soda" />
+        <input
+          type = "text"
+          name = "price"
+          placeholder = "Price per Pint" />
+        <button type = 'submit'>ADD IT UP.</button>
+      </form>
     </>
   );
 }
