19e6a369da180e9af681a819d29d0f0b1ec5d818
Ryan Bass <theryanbass@gmail.com>
2022-04-24 12:26:12 -0700

    add removePint handler

--- src/components/SodaControl.js
+++ src/components/SodaControl.js
@@ -7,15 +7,48 @@ export default class SodaControl extends React.Component {
     this.state = {
       sodas: [
         {
-          name: 'Wizzys Fizzies'
+          name: 'Dr. Salt',
+          brand: 'Wizzys Fizzies',
+          price: 4,
+          full: 124,
+          current: 124,
+          id: 1
+        },
+        {
+          name: 'Valley Dew',
+          brand: 'Bestseat Zola',
+          price: 4,
+          full: 124,
+          current: 124,
+          id: 2
+        },
+        {
+          name: 'Banta',
+          brand: 'Bestseat Zola',
+          price: 4.5,
+          full: 124,
+          current: 124,
+          id: 3
         }
       ]
     };
   }
+
+  handleSellPint = (id) => {
+    this.setState(prevState => {
+      return ({sodas: prevState.sodas.map(s => (s.id === id) &&
+        {...s, current: Math.max(s.current - 1, 0)} || {...s})})
+    });
+  }
+
+
   render(){
     return (
       <>
-      <p>test : {this.state.sodas[0].name}</p>
+      <SodaList
+      sodaList = {this.state.sodas}
+      sellPint = {this.handleSellPint}
+        />
       </>
     )
   }
