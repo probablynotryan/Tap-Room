a9e02712ea3a80308c05955e74b970a68b7d6d4a
Ryan Bass <theryanbass@gmail.com>
2022-04-24 10:45:03 -0700

    add sodacontrol

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,12 @@
 import '../App.css';
 import Header from './Header';
+import SodaControl from './SodaControl'
 
 function App() {
   return (
     <>
       <Header/>
+      <SodaControl />
     </>
   );
 }
